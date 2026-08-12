81479fc451777d1b069224b603209a6910d1386a
Rayat Chowdhury <rayatchowdhury2005@gmail.com>
2026-08-12 16:03:22 -0400

    Add: python-dynamic-typing - submission-0

---
+++ Python For Beginners/python-dynamic-typing/submission-0.py
@@ -0,0 +1,23 @@
+variable = 2 
+ 
+
+
+
+
+print(type(variable))
+
+variable = 32.2  
+
+print(type(variable)) 
+
+variable = True
+
+print(type(variable)) 
+
+variable = "Hi"
+
+print(type(variable)) 
+
+variable = [1,2,3]
+
+print(type(variable))
