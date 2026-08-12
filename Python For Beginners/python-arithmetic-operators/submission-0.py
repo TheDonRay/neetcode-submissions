39e53393f91ad53d43649b5699f7e143d3953d07
Rayat Chowdhury <rayatchowdhury2005@gmail.com>
2026-08-12 16:10:36 -0400

    Add: python-arithmetic-operators - submission-0

---
+++ Python For Beginners/python-arithmetic-operators/submission-0.py
@@ -0,0 +1,14 @@
+# note that the variables below are what we call assigning multiple variables 
+
+a, b, c = 2, 2, 0.5
+
+sumAllValues = a + b + c  
+
+product = a * b * c 
+#1 
+print(sumAllValues) 
+print(0 - sumAllValues)  
+print(a * b * c) 
+print(sumAllValues / product) 
+
+#2 
