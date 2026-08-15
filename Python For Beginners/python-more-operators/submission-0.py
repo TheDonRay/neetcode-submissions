4a92f1b90a79d4c4b99ffc4024a4522c4f5db614
Rayat Chowdhury <rayatchowdhury2005@gmail.com>
2026-08-15 18:46:53 -0400

    Add: python-more-operators - submission-0

---
+++ Python For Beginners/python-more-operators/submission-0.py
@@ -0,0 +1,20 @@
+a, b, c = 2, 8, 5 
+
+#handle product  
+product = a * b 
+
+#number 1 calculation 
+number1_calculation = ( a * b ) // c  
+print(number1_calculation) 
+
+#number 2 calculation 
+number2_remainder_value = (a*b) % c 
+print(number2_remainder_value) 
+
+#number 3 calculation 
+number3_raised = a**b 
+print(number3_raised)
+
+#number 4 calculation 
+number4_raised = b**c 
+print(number4_raised)
