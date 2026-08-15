c7e6ebe1580039336946ed06096d5eea2bd0cfdc
Rayat Chowdhury <rayatchowdhury2005@gmail.com>
2026-08-15 18:48:41 -0400

    Add: python-shorthand-operators - submission-0

---
+++ Python For Beginners/python-shorthand-operators/submission-0.py
@@ -0,0 +1,14 @@
+number = 0
+
+# short hand operators 
+#solving number 1 
+number += 5 
+print(number) 
+
+# number 2 
+number -= 2 
+print(number) 
+
+# number 3 
+number += number 
+print(number) 
