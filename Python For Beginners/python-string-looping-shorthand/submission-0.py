7da1614ab4b256f628c115e0be5a54a8edd43d1f
Rayat Chowdhury <rayatchowdhury2005@gmail.com>
2026-08-31 17:43:20 -0400

    Add: python-string-looping-shorthand - submission-0

---
+++ Python For Beginners/python-string-looping-shorthand/submission-0.py
@@ -0,0 +1,12 @@
+def print_string_characters(word1: str, word2: str) -> None:
+    #printing chars one seperate 
+    for char in word1: 
+        print(char) 
+    # print('First word characters done')
+    #second word 
+    for char in word2: 
+        print(char)
+    # print('Second word characters shown') 
+
+# do not modify below this line
+print_string_characters("Hello, World!", "Good Job!")
