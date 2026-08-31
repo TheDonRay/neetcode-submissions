9f020f51211993501a58405db9858a96d05ad8c9
Rayat Chowdhury <rayatchowdhury2005@gmail.com>
2026-08-31 14:22:10 -0400

    Add: python-nested-loops - submission-1

---
+++ Python For Beginners/python-nested-loops/submission-1.py
@@ -0,0 +1,5 @@
+#here what we can see here is the outer loop runs first then the inner loops start to implement.
+for i in range(3, 6): 
+    for j in range(3,6): 
+        #print the pair here as such 
+        print(i, j) 
