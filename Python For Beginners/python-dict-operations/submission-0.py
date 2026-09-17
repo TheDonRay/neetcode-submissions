ed3a5009af9a59ff51f088da6ea3affac13aba97
Rayat Chowdhury <rayatchowdhury2005@gmail.com>
2026-09-16 20:47:46 -0400

    Add: python-dict-operations - submission-0

---
+++ Python For Beginners/python-dict-operations/submission-0.py
@@ -0,0 +1,21 @@
+your_dict = { 
+  "a": 10, 
+  "apple": 12,
+  "bat": 7
+} 
+
+#printing the dictionary  
+print(your_dict) 
+
+#print the value of the key 
+print(your_dict["a"]) 
+
+# print true or false depending on whether the specific key is in the dictionary  
+print("d" in your_dict) 
+
+#reassign the value  
+your_dict["a"] = 4 
+
+#print the dictionary 
+print(your_dict)
+
